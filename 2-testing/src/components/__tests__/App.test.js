81541a86e380179ec4af7b80c66f60fb6f1d6f0c
Bryan C Guner <bryan.guner@gmail.com>
2023-04-25 15:34:07 -0400

    checking for comment list

--- 2-testing/src/components/__tests__/App.test.js
+++ 2-testing/src/components/__tests__/App.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { shallow } from "enzyme";
 import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 import App from "../App";
 
 it("shows a comment box", () => {
   const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
+
+it( "shows a comment list", () => {
+    const wrapped = shallow( <App /> );
+    expect( wrapped.find( CommentList ).length ).toEqual( 1 );
+    
+} );
