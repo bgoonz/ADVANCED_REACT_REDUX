de98ed47fced68af15994673b3d99cc68ebf0d85
Bryan C Guner <bryan.guner@gmail.com>
2023-04-25 13:54:47 -0400

    testing to see if comment box in app

--- 2-testing/src/components/__tests__/App.test.js
+++ 2-testing/src/components/__tests__/App.test.js
@@ -8,6 +8,9 @@ it( "shows a comment box", () => {
     ReactDOM.render( <App />, div );
     
     
+    // Looks inside the div
+    // and checks to see if the CommentBox is in there
+    expect( div.innerHTML ).toContain( "Comment Box" );
     
     //the following line is cleanup to remove the app component from memory
     ReactDOM.unmountComponentAtNode( div)
