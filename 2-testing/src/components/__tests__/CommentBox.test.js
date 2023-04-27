8e3157a0137f87e956888385a00ff21e52febe3b
Bryan C Guner <bryan.guner@gmail.com>
2023-04-27 12:15:29 -0400

    simulated submit

--- 2-testing/src/components/__tests__/CommentBox.test.js
+++ 2-testing/src/components/__tests__/CommentBox.test.js
@@ -19,9 +19,19 @@ it("has a text area and a button", () => {
 it("has a text area that users can type in", () => {
   wrapped.find("textarea").simulate("change", {
     target: { value: "new comment" },
-  } );
-    // Force the component to update
-    wrapped.update();
-    
-    expect( wrapped.find( "textarea" ).prop( "value" ) ).toEqual( "new comment" );
+  });
+  // Force the component to update
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it( "should empty the text area when the form is submitted", () => {
+      wrapped.find("textarea").simulate("change", {
+        target: { value: "new comment" },
+      } );
+      wrapped.update();
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
 });
