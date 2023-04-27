fdb37abed22b27b99c9b0258d74c9bfeb066af00
Bryan C Guner <bryan.guner@gmail.com>
2023-04-27 11:24:22 -0400

    afterEach unmount cleanup function

--- 2-testing/src/components/__tests__/CommentBox.test.js
+++ 2-testing/src/components/__tests__/CommentBox.test.js
@@ -2,17 +2,16 @@ import React from "react";
 import CommentBox from "components/CommentBox";
 import { mount } from "enzyme";
 
-
 let wrapped;
-beforeEach( () => {     
-    wrapped = mount( <CommentBox /> );
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
 } );
 
-it( "has a text area and a button", () => {
-
-    
-
-    
-    expect( wrapped.find( "textarea" ).length ).toEqual( 1 );
-    expect( wrapped.find( "button" ).length ).toEqual( 1 );
+afterEach( () => {
+    wrapped.unmount();
 } );
+
+it("has a text area and a button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
