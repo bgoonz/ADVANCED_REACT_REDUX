2e88f75c9943477040a2ccf0eb3e638bc0f871c9
Bryan C Guner <bryan.guner@gmail.com>
2023-04-27 11:46:15 -0400

    testing a fake event on commentbox component

--- 2-testing/src/components/__tests__/CommentBox.test.js
+++ 2-testing/src/components/__tests__/CommentBox.test.js
@@ -5,13 +5,22 @@ import { mount } from "enzyme";
 let wrapped;
 beforeEach(() => {
   wrapped = mount(<CommentBox />);
-} );
+});
 
-afterEach( () => {
-    wrapped.unmount();
-} );
+afterEach(() => {
+  wrapped.unmount();
+});
 
 it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
+});
+
+it("has a text area that users can type in", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  } );
+    wrapped.update();
+    
+    expect( wrapped.find( "textarea" ).prop( "value" ) ).toEqual( "new comment" );
 });
