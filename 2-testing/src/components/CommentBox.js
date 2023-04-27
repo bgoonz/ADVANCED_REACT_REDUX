8862cda76eb8a26a1072a7c61f49140ba124c3ef
Bryan C Guner <bryan.guner@gmail.com>
2023-04-27 11:07:11 -0400

    handle change and handle submit for comment box

--- 2-testing/src/components/CommentBox.js
+++ 2-testing/src/components/CommentBox.js
@@ -2,17 +2,30 @@ import React, {Component} from "react";
 
 class CommentBox extends Component{
     
+    state={comment: ""};
     
+    handleChange = ( event ) => {
+        this.setState( { comment: event.target.value } );
+    }
+    
+    handleSubmit = ( event ) => {
+        event.preventDefault();
+        
+        // TODO - Call an action creator
+        // And save the comment
+        
+        this.setState( { comment: "" } );
+    }
     render() {
         return (
-            <form>
-                <h4>Add a Comment</h4>
-                <textarea />
-                <div>
-                    <button>Submit Comment</button>
-                </div>
-            </form>
-        )
+          <form onSubmit={this.handleSubmit}>
+            <h4>Add a Comment</h4>
+            <textarea onChange={this.handleChange} value={this.state.comment} />
+            <div>
+              <button>Submit Comment</button>
+            </div>
+          </form>
+        );
     }
 }
 
