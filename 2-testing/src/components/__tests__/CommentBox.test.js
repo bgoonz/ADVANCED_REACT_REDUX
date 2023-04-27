b47cf87ad3167d69e53ab140939d601275041bed
Bryan C Guner <bryan.guner@gmail.com>
2023-04-27 12:54:35 -0400

    dry test by using describe block

--- 2-testing/src/components/__tests__/CommentBox.test.js
+++ 2-testing/src/components/__tests__/CommentBox.test.js
@@ -10,30 +10,30 @@ beforeEach(() => {
 afterEach(() => {
   wrapped.unmount();
 });
-
 it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
 
-it("has a text area that users can type in", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" },
-  });
-  // Force the component to update
-  wrapped.update();
+describe( 'the text area', () => {
+    beforeEach( () => {
+        wrapped.find( "textarea" ).simulate( "change", {
+            target: { value: "new comment" },
+        } );
+        // Force the component to update
+        wrapped.update();
+    } );
+    
+    it( "has a text area that users can type in", () => {
+        expect( wrapped.find( "textarea" ).prop( "value" ) ).toEqual( "new comment" );
+    } );
+    
+    it( "should empty the text area when the form is submitted", () => {
+        wrapped.find( "form" ).simulate( "submit" );
+        wrapped.update();
+        expect( wrapped.find( "textarea" ).prop( "value" ) ).toEqual( "" );
+    } );
+} );
 
-  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
-});
 
-it("should empty the text area when the form is submitted", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" },
-  });
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 
-  wrapped.find("form").simulate("submit");
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("");
-});
