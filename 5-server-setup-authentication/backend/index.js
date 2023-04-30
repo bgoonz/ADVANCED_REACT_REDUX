8ff6a89febf21dbf90e89d4dc6355d385bb68b97
Bryan C Guner <bryan.guner@gmail.com>
2023-04-30 19:29:16 -0400

    attempted mongodb setup

--- 5-server-setup-authentication/backend/index.js
+++ 5-server-setup-authentication/backend/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const http = require("http");
-const bodyParser = require("body-parser");
+const bodyParser = require( "body-parser" );
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const router = require("./router");
+
+// DB Setup
+mongoose
+  .connect(
+    `mongodb+srv://bryanguner:Summer2015@cluster0.oj1ibrz.mongodb.net/auth?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
 
 // an instance of express
 const app = express();
@@ -18,3 +34,5 @@ const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
 console.log("Server listening on:", port);
+
+
