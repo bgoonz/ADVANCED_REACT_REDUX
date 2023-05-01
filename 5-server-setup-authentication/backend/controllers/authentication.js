c7022379383ac93635a1b91e9a48136a40b0c329
Bryan C Guner <bryan.guner@gmail.com>
2023-05-01 11:34:23 -0400

    creating user records

--- 5-server-setup-authentication/backend/controllers/authentication.js
+++ 5-server-setup-authentication/backend/controllers/authentication.js
@@ -4,27 +4,27 @@ exports.signup = async (req, res, next) => {
   const password = req.body.password;
 
   // see if a user with the given email exists
-    let existingUser;
-    try {
-        existingUser = await User.findOne( { email: email } );
-    } catch ( err ) {
-        return next( err );
-    }
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    return next(err);
+  }
 
-    // if a user with email does exist, return an error
-    if (existingUser) {
-      return res.status(422).send({ error: "Email is in use" });
-    }
+  // if a user with email does exist, return an error
+  if (existingUser) {
+    return res.status(422).send({ error: "Email is in use" });
+  }
 
-    // if a user with email does NOT exist, create and save user record
-    const user = new User({
-      email: email,
-      password: password,
-    } );
-    try{
-       await user.save();
-    } catch ( err ) {
-        return next( err );
-    }
-
+  // if a user with email does NOT exist, create and save user record
+  const user = new User({
+    email: email,
+    password: password,
+  });
+  try {
+    await user.save();
+  } catch (err) {
+    return next(err);
+  }
+  res.json({ success: true });
 };
