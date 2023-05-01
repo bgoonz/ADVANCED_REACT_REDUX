1a5c9e184dc1d9d4302c605fd5f760d13ba7d5e1
Bryan C Guner <bryan.guner@gmail.com>
2023-05-01 11:08:35 -0400

    signup is working with mongoDB:

--- 5-server-setup-authentication/backend/controllers/authentication.js
+++ 5-server-setup-authentication/backend/controllers/authentication.js
@@ -4,12 +4,27 @@ exports.signup = async (req, res, next) => {
   const password = req.body.password;
 
   // see if a user with the given email exists
-  User.findOne({ email: email }, (err, existingUser) => {
-    if (err) {
-      return next(err);
+    let existingUser;
+    try {
+        existingUser = await User.findOne( { email: email } );
+    } catch ( err ) {
+        return next( err );
     }
-  });
-  // if a user with email does exist, return an error
-  // if a user with email does NOT exist, create and save user record
-  // respond to request indicating the user was created
+
+    // if a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: "Email is in use" });
+    }
+
+    // if a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password,
+    } );
+    try{
+       await user.save();
+    } catch ( err ) {
+        return next( err );
+    }
+
 };
