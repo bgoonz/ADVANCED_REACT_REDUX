aadd9369f4627ae08320715264ca6639b783211c
Bryan C Guner <bryan.guner@gmail.com>
2023-05-02 15:11:34 -0400

    purpose of and work on local strategy for signing in an existing user

--- 5-server-setup-authentication/backend/services/passport.js
+++ 5-server-setup-authentication/backend/services/passport.js
@@ -3,8 +3,7 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const LocalStrategy = require( 'passport-local' );
-
+const LocalStrategy = require("passport-local");
 
 // set up options for JWT strategy
 const jwtOptions = {
@@ -12,18 +11,38 @@ const jwtOptions = {
   secretOrKey: config.secret,
 };
 
-// create local strategy
-const localOptions = { usernameField: 'email' };
-const localLogin = new LocalStrategy( localOptions, function ( email, password, done ) {
-    // verify this email and password, call done with the user
-    
-    
-    // if it is the correct email and password
-    // otherwise, call done with false
-    
+// create local strategy (for signing in)
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function (
+  email,
+  password,
+  done
+) {
+  // verify this email and password, call done with the user
+  // if it is the correct email and password
+  // otherwise, call done with false
 
+  User.findOne({ email: email })
+    .then(function (user) {
+      if (!user) {
+        return done(null, false);
+      }
 
-} );
+      // compare passwords - is `password`...(supplied by request) equal to user.password?
+      user.comparePassword(password, function (err, isMatch) {
+        if (err) {
+          return done(err);
+        }
+
+        if (!isMatch) {
+          return done(null, false);
+        }
+
+        return done(null, user);
+      });
+    })
+    .catch(function (err) {});
+});
 
 // create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
