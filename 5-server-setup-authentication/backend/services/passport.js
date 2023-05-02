4ca07bfcf85a5386307bee86ff7d9207cc08f3d0
Bryan C Guner <bryan.guner@gmail.com>
2023-05-02 15:04:50 -0400

    signing in with local strategy

--- 5-server-setup-authentication/backend/services/passport.js
+++ 5-server-setup-authentication/backend/services/passport.js
@@ -3,12 +3,27 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require( 'passport-local' );
+
 
 // set up options for JWT strategy
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader("authorization"),
   secretOrKey: config.secret,
 };
+
+// create local strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy( localOptions, function ( email, password, done ) {
+    // verify this email and password, call done with the user
+    
+    
+    // if it is the correct email and password
+    // otherwise, call done with false
+    
+
+
+} );
 
 // create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
